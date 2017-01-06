3ab04ed5559fe37345b27d535cc50eb25e29827a
Stefano Gerli <sgerli@igerli.com>
2017-01-06 11:20:25 -0600

    Added Resin.io Docker

---
+++ app/start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# using local electron module instead of the global electron lets you
+# easily control specific version dependency between your app and electron itself.
+# the syntax below starts an X istance with ONLY our electronJS fired up,
+# it saves you a LOT of resources avoiding full-desktops envs
+
+rm /tmp/.X0-lock &>/dev/null || true
+startx /usr/src/app/node_modules/electron/dist/electron /usr/src/app --enable-logging
