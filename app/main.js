0ca40d86f02aa7563017c5ee8ffe7234a959d4be
Stefano Gerli <sgerli@igerli.com>
2017-02-01 13:58:05 -0600

    Update all

--- app/main.js
+++ app/main.js
@@ -1,100 +1,92 @@
-#!/bin/env node
-{
-    var piPins =  require("pi-pins");
 
-    require('require-rebuild')();
-    const electron = require('electron');
-    const app = electron.app;
-    const BrowserWindow = electron.BrowserWindow;
+var piPins =  require("pi-pins");
 
-    // var gpio = require('rpi-gpio');
-    var button = piPins.connect(16);
-    button.mode('in')
-    button.on('rise', function () {
-        console.log("Button Pressed");
-    });
+require('require-rebuild')();
+const electron = require('electron');
 
-    // GPIO Detection
-    /* Button Pins
-    //////////////
-    // Up = 16
-    // Down = 17
-    // Back = 27
-    // Select = 22
-    /////////////*/
+const { app, BrowserWindow } = electron;
 
-    /*gpio.on('change', function(channel, value) {
-        console.log('Channel ' + channel + ' value is now ' + value);
-    });
-    gpio.setup(16, gpio.DIR_IN, gpio.EDGE_BOTH);
-    gpio.setup(17, gpio.DIR_IN, gpio.EDGE_BOTH);
-    gpio.setup(27, gpio.DIR_IN, gpio.EDGE_BOTH);
-    gpio.setup(22, gpio.DIR_IN, gpio.EDGE_BOTH);*/
-
-    // simple parameters initialization
-    let electronConfig = {
-        "URL_LAUNCHER_TOUCH": process.env.URL_LAUNCHER_TOUCH == null ? 0 : process.env.URL_LAUNCHER_TOUCH === '1',
-        "URL_LAUNCHER_TOUCH_SIMULATE": process.env.URL_LAUNCHER_TOUCH_SIMULATE == null ? 0 : process.env.URL_LAUNCHER_TOUCH_SIMULATE === '1',
-        "URL_LAUNCHER_FRAME": process.env.URL_LAUNCHER_FRAME == null ? 0 : process.env.URL_LAUNCHER_FRAME === '1',
-        "URL_LAUNCHER_KIOSK": process.env.URL_LAUNCHER_KIOSK == null ? 1 : process.env.URL_LAUNCHER_KIOSK === '1',
-        "URL_LAUNCHER_NODE": process.env.URL_LAUNCHER_NODE == null ? 0 : process.env.URL_LAUNCHER_NODE === '1',
-        "URL_LAUNCHER_WIDTH": (process.env.URL_LAUNCHER_WIDTH == null) ? 1920 : parseInt(process.env.URL_LAUNCHER_WIDTH),
-        "URL_LAUNCHER_HEIGHT": (process.env.URL_LAUNCHER_HEIGHT == null) ? 1080 : parseInt(process.env.URL_LAUNCHER_HEIGHT),
-        "URL_LAUNCHER_TITLE": (process.env.URL_LAUNCHER_TITLE == null) ? "RESIN.IO" : process.env.URL_LAUNCHER_TITLE,
-        "URL_LAUNCHER_CONSOLE": process.env.URL_LAUNCHER_CONSOLE == null ? 0 : process.env.URL_LAUNCHER_CONSOLE === '1',
-        "URL_LAUNCHER_URL": (process.env.URL_LAUNCHER_URL == null) ? "file:////usr/src/app/data/index.html" : process.env.URL_LAUNCHER_URL,
-        "URL_LAUNCHER_ZOOM": (process.env.URL_LAUNCHER_ZOOM == null) ? 1.0 : parseFloat(process.env.URL_LAUNCHER_ZOOM),
-        "URL_LAUNCHER_OVERLAY_SCROLLBARS": process.env.URL_LAUNCHER_CONSOLE == null ? 0 : process.env.URL_LAUNCHER_CONSOLE === '1'
-    };
-
-    let window = null;
-
-    // Allow also webviews, along window, to load from fs
-    app.commandLine.appendSwitch("--allow-file-access-from-files");
-
-    // enable touch events if your device supports them
-    if (electronConfig.URL_LAUNCHER_TOUCH) {
-        app.commandLine.appendSwitch("--touch-devices");
-    }
-    // simulate touch events - might be useful for touchscreen with partial driver support
-    if (electronConfig.URL_LAUNCHER_TOUCH_SIMULATE) {
-        app.commandLine.appendSwitch("--simulate-touch-screen-with-mouse");
-    }
-
-    /*
-      we initialize our application display as a callback of the electronJS "ready" event
-    */
-    app.on('ready', () => {
-        'use strict';
-        // here we actually configure the behavour of electronJS
-        window = new BrowserWindow({
-            width: parseInt(electronConfig.URL_LAUNCHER_WIDTH),
-            height: parseInt(electronConfig.URL_LAUNCHER_HEIGHT),
-            frame: (electronConfig.URL_LAUNCHER_FRAME) ? true : false,
-            title: electronConfig.URL_LAUNCHER_TITLE,
-            kiosk: (electronConfig.URL_LAUNCHER_KIOSK) ? true : false,
-            webPreferences: {
-                nodeIntegration: (electronConfig.URL_LAUNCHER_NODE) ? true : false,
-                zoomFactor: electronConfig.URL_LAUNCHER_ZOOM,
-                overlayScrollbars: (electronConfig.URL_LAUNCHER_OVERLAY_SCROLLBARS) ? true : false
-            }
-        });
-
-        window.webContents.on('did-finish-load', () => {
-            setTimeout(() => {
-                window.show();
-            }, 300);
-
-        });
-
-        // if the env-var is set to true, a portion of the screen will be dedicated to the chrome-dev-tools
-        if (electronConfig.URL_LAUNCHER_CONSOLE) {
-            window.openDevTools();
-        }
-
-        // the big red button, here we go
-        window.loadURL(electronConfig.URL_LAUNCHER_URL);
+// var gpio = require('rpi-gpio');
 
 
-    });
+// GPIO Detection
+/* Button Pins
+//////////////
+// Up = 16
+// Down = 17
+// Back = 27
+// Select = 22
+/////////////*/
+
+/*gpio.on('change', function(channel, value) {
+    console.log('Channel ' + channel + ' value is now ' + value);
+});
+gpio.setup(16, gpio.DIR_IN, gpio.EDGE_BOTH);
+gpio.setup(17, gpio.DIR_IN, gpio.EDGE_BOTH);
+gpio.setup(27, gpio.DIR_IN, gpio.EDGE_BOTH);
+gpio.setup(22, gpio.DIR_IN, gpio.EDGE_BOTH);*/
+
+// simple parameters initialization
+const electronConfig = {
+  URL_LAUNCHER_TOUCH: process.env.URL_LAUNCHER_TOUCH === '1' ? 1 : 0,
+  URL_LAUNCHER_TOUCH_SIMULATE: process.env.URL_LAUNCHER_TOUCH_SIMULATE === '1' ? 1 : 0,
+  URL_LAUNCHER_FRAME: process.env.URL_LAUNCHER_FRAME === '1' ? 1 : 0,
+  URL_LAUNCHER_KIOSK: process.env.URL_LAUNCHER_KIOSK === '1' ? 1 : 0,
+  URL_LAUNCHER_NODE: process.env.URL_LAUNCHER_NODE === '1' ? 1 : 0,
+  URL_LAUNCHER_WIDTH: parseInt(process.env.URL_LAUNCHER_WIDTH || 1920, 10),
+  URL_LAUNCHER_HEIGHT: parseInt(process.env.URL_LAUNCHER_HEIGHT || 1080, 10),
+  URL_LAUNCHER_TITLE: process.env.URL_LAUNCHER_TITLE || 'RESIN.IO',
+  URL_LAUNCHER_CONSOLE: process.env.URL_LAUNCHER_CONSOLE === '1' ? 1 : 0,
+  URL_LAUNCHER_URL: process.env.URL_LAUNCHER_URL || 'file:////usr/src/app/data/index.html',
+  URL_LAUNCHER_ZOOM: parseFloat(process.env.URL_LAUNCHER_ZOOM || 1.0),
+  URL_LAUNCHER_OVERLAY_SCROLLBARS: process.env.URL_LAUNCHER_CONSOLE === '1' ? 1 : 0,
+};
+
+// enable touch events if your device supports them
+if (electronConfig.URL_LAUNCHER_TOUCH) {
+  app.commandLine.appendSwitch('--touch-devices');
 }
+// simulate touch events - might be useful for touchscreen with partial driver support
+if (electronConfig.URL_LAUNCHER_TOUCH_SIMULATE) {
+  app.commandLine.appendSwitch('--simulate-touch-screen-with-mouse');
+}
+
+/*
+ we initialize our application display as a callback of the electronJS "ready" event
+ */
+app.on('ready', () => {
+  // here we actually configure the behavour of electronJS
+  const window = new BrowserWindow({
+    width: electronConfig.URL_LAUNCHER_WIDTH,
+    height: electronConfig.URL_LAUNCHER_HEIGHT,
+    frame: !!(electronConfig.URL_LAUNCHER_FRAME),
+    title: electronConfig.URL_LAUNCHER_TITLE,
+    kiosk: !!(electronConfig.URL_LAUNCHER_KIOSK),
+    webPreferences: {
+      nodeIntegration: !!(electronConfig.URL_LAUNCHER_NODE),
+      zoomFactor: electronConfig.URL_LAUNCHER_ZOOM,
+      overlayScrollbars: !!(electronConfig.URL_LAUNCHER_OVERLAY_SCROLLBARS),
+    },
+  });
+
+  var button = piPins.connect(16);
+  button.mode('in')
+  button.on('rise', function () {
+      console.log("Button Pressed");
+  });
+
+  window.webContents.on('did-finish-load', () => {
+    setTimeout(() => {
+      window.show();
+    }, 300);
+  });
+
+  // if the env-var is set to true,
+  // a portion of the screen will be dedicated to the chrome-dev-tools
+  if (electronConfig.URL_LAUNCHER_CONSOLE) {
+    window.openDevTools();
+  }
+
+  // the big red button, here we go
+  window.loadURL(electronConfig.URL_LAUNCHER_URL);
+});
