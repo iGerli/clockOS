2fb447a30f5c06b78d97122cebe599d3c4049efe
Stefano Gerli <sgerli@igerli.com>
2017-02-01 08:24:13 -0600

    New build with hash strategy

--- app/data/main.bundle.js
+++ app/data/main.bundle.js
@@ -175,7 +175,7 @@ var AppRoutingModule = (function () {
     AppRoutingModule = __decorate([
         __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["b" /* NgModule */])({
             imports: [
-                __WEBPACK_IMPORTED_MODULE_1__angular_router__["a" /* RouterModule */].forRoot(appRoutes)
+                __WEBPACK_IMPORTED_MODULE_1__angular_router__["a" /* RouterModule */].forRoot(appRoutes, { useHash: true })
             ],
             exports: [
                 __WEBPACK_IMPORTED_MODULE_1__angular_router__["a" /* RouterModule */]
