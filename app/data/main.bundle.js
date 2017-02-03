fafe01afd5eae1a5dd5656d103f236bd824337fc
Stefano Gerli <sgerli@igerli.com>
2017-02-03 14:20:53 -0600

    Updated to new menu and cleaned fonts

--- app/data/main.bundle.js
+++ app/data/main.bundle.js
@@ -57,8 +57,8 @@ var HomeComponent = (function () {
     HomeComponent = __decorate([
         __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["i" /* Component */])({
             selector: 'app-home',
-            template: __webpack_require__(802),
-            styles: [__webpack_require__(798)],
+            template: __webpack_require__(804),
+            styles: [__webpack_require__(799)],
             animations: [__WEBPACK_IMPORTED_MODULE_2__animations__["a" /* fadeInZoom */]]
         }), 
         __metadata('design:paramtypes', [(typeof (_b = typeof __WEBPACK_IMPORTED_MODULE_0__angular_core__["C" /* Renderer */] !== 'undefined' && __WEBPACK_IMPORTED_MODULE_0__angular_core__["C" /* Renderer */]) === 'function' && _b) || Object, (typeof (_c = typeof __WEBPACK_IMPORTED_MODULE_1__angular_router__["b" /* Router */] !== 'undefined' && __WEBPACK_IMPORTED_MODULE_1__angular_router__["b" /* Router */]) === 'function' && _c) || Object])
@@ -94,8 +94,8 @@ var MenuComponent = (function () {
     MenuComponent = __decorate([
         __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["i" /* Component */])({
             selector: 'app-menu',
-            template: __webpack_require__(803),
-            styles: [__webpack_require__(799)]
+            template: __webpack_require__(805),
+            styles: [__webpack_require__(800)]
         }), 
         __metadata('design:paramtypes', [])
     ], MenuComponent);
@@ -124,10 +124,10 @@ webpackEmptyContext.id = 478;
 
 "use strict";
 Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__polyfills_ts__ = __webpack_require__(643);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__polyfills_ts__ = __webpack_require__(644);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_platform_browser_dynamic__ = __webpack_require__(607);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__angular_core__ = __webpack_require__(0);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__environments_environment__ = __webpack_require__(642);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__environments_environment__ = __webpack_require__(643);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__app_app_module__ = __webpack_require__(640);
 
 
@@ -264,8 +264,8 @@ var AppComponent = (function () {
     AppComponent = __decorate([
         __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["i" /* Component */])({
             selector: 'app-root',
-            template: __webpack_require__(800),
-            styles: [__webpack_require__(796)]
+            template: __webpack_require__(802),
+            styles: [__webpack_require__(797)]
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
@@ -289,6 +289,7 @@ var AppComponent = (function () {
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_7__clock_clock_component__ = __webpack_require__(641);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_8__home_home_component__ = __webpack_require__(425);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_9__menu_menu_component__ = __webpack_require__(426);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_10__status_bar_status_bar_component__ = __webpack_require__(642);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppModule; });
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
@@ -309,6 +310,7 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 
 
 
+
 var AppModule = (function () {
     function AppModule() {
     }
@@ -318,7 +320,8 @@ var AppModule = (function () {
                 __WEBPACK_IMPORTED_MODULE_5__app_component__["a" /* AppComponent */],
                 __WEBPACK_IMPORTED_MODULE_7__clock_clock_component__["a" /* ClockComponent */],
                 __WEBPACK_IMPORTED_MODULE_8__home_home_component__["a" /* HomeComponent */],
-                __WEBPACK_IMPORTED_MODULE_9__menu_menu_component__["a" /* MenuComponent */]
+                __WEBPACK_IMPORTED_MODULE_9__menu_menu_component__["a" /* MenuComponent */],
+                __WEBPACK_IMPORTED_MODULE_10__status_bar_status_bar_component__["a" /* StatusBarComponent */]
             ],
             imports: [
                 __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__["a" /* BrowserModule */],
@@ -382,8 +385,8 @@ var ClockComponent = (function () {
     ClockComponent = __decorate([
         __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["i" /* Component */])({
             selector: 'clock-widget',
-            template: __webpack_require__(801),
-            styles: [__webpack_require__(797)]
+            template: __webpack_require__(803),
+            styles: [__webpack_require__(798)]
         }), 
         __metadata('design:paramtypes', [])
     ], ClockComponent);
@@ -394,6 +397,41 @@ var ClockComponent = (function () {
 /***/ }),
 
 /***/ 642:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return StatusBarComponent; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+var StatusBarComponent = (function () {
+    function StatusBarComponent() {
+    }
+    StatusBarComponent.prototype.ngOnInit = function () {
+    };
+    StatusBarComponent = __decorate([
+        __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["i" /* Component */])({
+            selector: 'app-status-bar',
+            template: __webpack_require__(806),
+            styles: [__webpack_require__(801)]
+        }), 
+        __metadata('design:paramtypes', [])
+    ], StatusBarComponent);
+    return StatusBarComponent;
+}());
+//# sourceMappingURL=/home/sgerli/Development/iGerli Clock/clockOS-ui/src/status-bar.component.js.map
+
+/***/ }),
+
+/***/ 643:
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
@@ -409,41 +447,41 @@ var environment = {
 
 /***/ }),
 
-/***/ 643:
+/***/ 644:
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_core_js_es6_symbol__ = __webpack_require__(657);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_core_js_es6_symbol__ = __webpack_require__(658);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_core_js_es6_symbol___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_core_js_es6_symbol__);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_core_js_es6_object__ = __webpack_require__(650);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_core_js_es6_object__ = __webpack_require__(651);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_core_js_es6_object___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1_core_js_es6_object__);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_core_js_es6_function__ = __webpack_require__(646);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_core_js_es6_function__ = __webpack_require__(647);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_core_js_es6_function___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_2_core_js_es6_function__);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3_core_js_es6_parse_int__ = __webpack_require__(652);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3_core_js_es6_parse_int__ = __webpack_require__(653);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_3_core_js_es6_parse_int___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_3_core_js_es6_parse_int__);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4_core_js_es6_parse_float__ = __webpack_require__(651);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4_core_js_es6_parse_float__ = __webpack_require__(652);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_4_core_js_es6_parse_float___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_4_core_js_es6_parse_float__);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5_core_js_es6_number__ = __webpack_require__(649);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5_core_js_es6_number__ = __webpack_require__(650);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_5_core_js_es6_number___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_5_core_js_es6_number__);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6_core_js_es6_math__ = __webpack_require__(648);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6_core_js_es6_math__ = __webpack_require__(649);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_6_core_js_es6_math___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_6_core_js_es6_math__);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_7_core_js_es6_string__ = __webpack_require__(656);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_7_core_js_es6_string__ = __webpack_require__(657);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_7_core_js_es6_string___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_7_core_js_es6_string__);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_8_core_js_es6_date__ = __webpack_require__(645);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_8_core_js_es6_date__ = __webpack_require__(646);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_8_core_js_es6_date___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_8_core_js_es6_date__);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_9_core_js_es6_array__ = __webpack_require__(644);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_9_core_js_es6_array__ = __webpack_require__(645);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_9_core_js_es6_array___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_9_core_js_es6_array__);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_10_core_js_es6_regexp__ = __webpack_require__(654);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_10_core_js_es6_regexp__ = __webpack_require__(655);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_10_core_js_es6_regexp___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_10_core_js_es6_regexp__);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_11_core_js_es6_map__ = __webpack_require__(647);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_11_core_js_es6_map__ = __webpack_require__(648);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_11_core_js_es6_map___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_11_core_js_es6_map__);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_12_core_js_es6_set__ = __webpack_require__(655);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_12_core_js_es6_set__ = __webpack_require__(656);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_12_core_js_es6_set___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_12_core_js_es6_set__);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_13_core_js_es6_reflect__ = __webpack_require__(653);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_13_core_js_es6_reflect__ = __webpack_require__(654);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_13_core_js_es6_reflect___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_13_core_js_es6_reflect__);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_14_core_js_es7_reflect__ = __webpack_require__(658);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_14_core_js_es7_reflect__ = __webpack_require__(659);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_14_core_js_es7_reflect___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_14_core_js_es7_reflect__);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_15_zone_js_dist_zone__ = __webpack_require__(845);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_15_zone_js_dist_zone__ = __webpack_require__(848);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_15_zone_js_dist_zone___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_15_zone_js_dist_zone__);
 
 
@@ -465,24 +503,17 @@ var environment = {
 
 /***/ }),
 
-/***/ 796:
-/***/ (function(module, exports) {
-
-module.exports = ""
-
-/***/ }),
-
 /***/ 797:
 /***/ (function(module, exports) {
 
-module.exports = ".hour.md-display-3 {\n  font-size: 80px;\n  line-height: 68px;\n  margin: 0;\n  font-weight: 100; }\n\n.date.md-display-0 {\n  margin: 0;\n  margin-bottom: 20px; }\n\n/* AM/PM Indicator */\n.indicator.md-display-0 {\n  margin-right: 20px; }\n\n.md-display-0 {\n  font-weight: 300;\n  font-size: 24px;\n  line-height: 26px;\n  margin: 5px 0; }\n"
+module.exports = ""
 
 /***/ }),
 
 /***/ 798:
 /***/ (function(module, exports) {
 
-module.exports = ""
+module.exports = ".hour.md-display-3 {\n  font-size: 80px;\n  line-height: 68px;\n  margin: 0;\n  font-weight: 100; }\n\n.date.md-display-0 {\n  margin: 0;\n  margin-bottom: 20px; }\n\n/* AM/PM Indicator */\n.indicator.md-display-0 {\n  margin-right: 20px; }\n\n.md-display-0 {\n  font-weight: 300;\n  font-size: 24px;\n  line-height: 26px;\n  margin: 5px 0; }\n  .md-display-0.status-clock {\n    font-size: 16px;\n    font-weight: 400;\n    line-height: 20px; }\n"
 
 /***/ }),
 
@@ -496,32 +527,53 @@ module.exports = ""
 /***/ 800:
 /***/ (function(module, exports) {
 
-module.exports = "<router-outlet></router-outlet>"
+module.exports = "md-list a {\n  text-decoration: none; }\n  md-list a:focus {\n    outline: none; }\n    md-list a:focus md-list-item {\n      background: #009688;\n      color: #fff; }\n"
 
 /***/ }),
 
 /***/ 801:
 /***/ (function(module, exports) {
 
-module.exports = "<h1 [class]=\"extraClass + ' md-display-' + size\" [innerText]=\"clock | date: format\">clock</h1>\n"
+module.exports = ""
 
 /***/ }),
 
 /***/ 802:
 /***/ (function(module, exports) {
 
-module.exports = "<div class=\"flex-container\">\n  <div class=\"full-width\">\n    <clock-widget [extraClass]=\"'align-left date'\" [size]=\"0\" [format]=\"'EEEE, MMMM d'\"></clock-widget>\n    <clock-widget [extraClass]=\"'hour align-center'\" [size]=\"3\" [format]=\"'h:mm'\"></clock-widget>\n    <clock-widget [extraClass]=\"'align-right indicator'\" [size]=\"0\" [format]=\"'a'\"></clock-widget>\n  </div>\n</div>\n<button #mainButton class=\"hidden-button\" (blur)=\"buttonBlur()\" (click)=\"onEnter()\"></button>"
+module.exports = "<router-outlet></router-outlet>"
 
 /***/ }),
 
 /***/ 803:
 /***/ (function(module, exports) {
 
-module.exports = "<div class=\"md-app-background\">\n  <md-list>\n  <md-list-item><a href=\"#\">Pepper</a></md-list-item>\n  <a href=\"#\"><md-list-item> Salt </md-list-item></a>\n  <md-list-item> Paprika </md-list-item>\n  </md-list>\n</div>\n"
+module.exports = "<h1 [class]=\"extraClass + ' md-display-' + size\" [innerText]=\"clock | date: format\">clock</h1>\n"
 
 /***/ }),
 
-/***/ 846:
+/***/ 804:
+/***/ (function(module, exports) {
+
+module.exports = "<div class=\"flex-container\">\n  <div class=\"full-width\">\n    <clock-widget [extraClass]=\"'align-left date'\" [size]=\"0\" [format]=\"'EEEE, MMMM d'\"></clock-widget>\n    <clock-widget [extraClass]=\"'hour align-center'\" [size]=\"3\" [format]=\"'h:mm'\"></clock-widget>\n    <clock-widget [extraClass]=\"'align-right indicator'\" [size]=\"0\" [format]=\"'a'\"></clock-widget>\n  </div>\n</div>\n<button #mainButton class=\"hidden-button\" (blur)=\"buttonBlur()\" (click)=\"onEnter()\"></button>"
+
+/***/ }),
+
+/***/ 805:
+/***/ (function(module, exports) {
+
+module.exports = "<div class=\"white-app-background\">\n  <app-status-bar></app-status-bar>\n  <md-list>\n    <a href=\"#\"><md-list-item>Pepper</md-list-item></a>\n    <a href=\"#\"><md-list-item> Salt </md-list-item></a>\n    <a href=\"#\"><md-list-item> Paprika </md-list-item></a>\n  </md-list>\n</div>\n"
+
+/***/ }),
+
+/***/ 806:
+/***/ (function(module, exports) {
+
+module.exports = "<md-toolbar class=\"center-text\" color=\"primary\">\n  <span><clock-widget [extraClass]=\"'status-clock'\" [size]=\"0\" [format]=\"'h:mm a'\"></clock-widget></span>\n</md-toolbar>"
+
+/***/ }),
+
+/***/ 849:
 /***/ (function(module, exports, __webpack_require__) {
 
 module.exports = __webpack_require__(479);
@@ -529,5 +581,5 @@ module.exports = __webpack_require__(479);
 
 /***/ })
 
-},[846]);
+},[849]);
 //# sourceMappingURL=main.bundle.map
